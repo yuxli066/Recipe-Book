f53cc866230c30589f58bdc1b1cf6c00df718eb8
Leo Li <yuxli066@gmail.com>
2021-12-02 13:24:56 -0800

    added placeholder data for recipe + simple CRUD routes

--- server/controllers/root-ctrl.js
+++ server/controllers/root-ctrl.js
@@ -10,12 +10,86 @@ class RootCtrl {
     });
   }
 
-  static async getMany(req, res) {
-    // TODO: access database here for all recipes
-    return res.json({});
+  // TODO: access database here for all recipes
+  static async getRecipes(req, res) {
+    return res.json([
+      {
+        recipeName: "Name1 Placeholder",
+        description: "Description Placeholder",
+        rating: 5,
+        instructions: {
+          1: "step 1",
+          2: "step 2",
+          3: "step 3",
+          4: "step 4",
+          5: "step 5",
+        },
+      },
+      {
+        recipeName: "Name2 Placeholder",
+        description: "Description Placeholder",
+        rating: 5,
+        instructions: {
+          1: "step 1",
+          2: "step 2",
+          3: "step 3",
+          4: "step 4",
+          5: "step 5",
+        },
+      },
+      {
+        recipeName: "Name3 Placeholder",
+        description: "Description Placeholder",
+        rating: 5,
+        instructions: {
+          1: "step 1",
+          2: "step 2",
+          3: "step 3",
+          4: "step 4",
+          5: "step 5",
+        },
+      },
+      {
+        recipeName: "Name4 Placeholder",
+        description: "Description Placeholder",
+        rating: 5,
+        instructions: {
+          1: "step 1",
+          2: "step 2",
+          3: "step 3",
+          4: "step 4",
+          5: "step 5",
+        },
+      },
+      {
+        recipeName: "Name5 Placeholder",
+        description: "Description Placeholder",
+        rating: 5,
+        instructions: {
+          1: "step 1",
+          2: "step 2",
+          3: "step 3",
+          4: "step 4",
+          5: "step 5",
+        },
+      },
+      {
+        recipeName: "Name6 Placeholder",
+        description: "Description Placeholder",
+        rating: 5,
+        instructions: {
+          1: "step 1",
+          2: "step 2",
+          3: "step 3",
+          4: "step 4",
+          5: "step 5",
+        },
+      },
+    ]);
   }
 
-  static async post(req, res) {
+  // TODO: save new recipe to database
+  static async newRecipe(req, res) {
     return res.created({ message: "create new data" });
   }
 }
--- server/routers/root-route.js
+++ server/routers/root-route.js
@@ -4,7 +4,7 @@ const RootRt = require("express").Router();
 const { RootCtrl } = require("../controllers");
 
 RootRt.get("/healthCheck", RootCtrl.getHealthCheck);
-RootRt.get("/v1/recipes", RootCtrl.post);
-RootRt.post("/v1/newRecipe", RootCtrl.post);
+RootRt.get("/v1/recipes", RootCtrl.getRecipes);
+RootRt.post("/v1/newRecipe", RootCtrl.newRecipe);
 
 module.exports = RootRt;
