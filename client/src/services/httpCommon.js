42a3326145a8aee38f81d67f282caf004ab2330f
leoli7405@gmail.com <leoli7405@gmail.com>
2024-03-12 17:45:28 -0700

    fixed bugs

--- client/src/services/httpCommon.js
+++ client/src/services/httpCommon.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 // constant variables
-const base = (process.env.NODE_ENV = "dev"
-  ? "http://localhost:3000/v1/"
-  : "http://20.102.36.29:5000/v1/");
+const base =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:3001/v1/"
+    : "http://20.102.36.29:5000/v1/";
 
 export default axios.create({
   baseURL: base,
