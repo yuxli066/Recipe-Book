a1a281bbe31a91ce8823254652eb8b9808d466e9
leoli7405@gmail.com <leoli7405@gmail.com>
2024-03-12 19:18:03 -0700

    added recipe images

--- scripts/sqlUpload.js
+++ scripts/sqlUpload.js
@@ -4,10 +4,19 @@ const recipes = require("./recipes.json");
 (async () => {
   const db = new databaseService();
   for (let recipe of recipes) {
+    const toDataURL = (url) => {
+      const file_contents = fs.readFileSync(url);
+      const response_64 = new Buffer.from(file_contents).toString("base64");
+      return response_64;
+    };
+    const image_buffer = toDataURL(recipe.image);
+    recipe.image = image_buffer;
+
     await db.insertNewRecipe(recipe, (err, rowCount) => {
       if (err) console.error(err);
       else console.log("Row Count:", rowCount);
     });
   }
+
   await db.closeConnection();
 })();
