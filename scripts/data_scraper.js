c45cbfbaa06958156856caed3432d56551f12b70
leoli7405@gmail.com <leoli7405@gmail.com>
2024-03-23 15:37:12 -0700

    scraped photos & completed scraper

--- scripts/data_scraper.js
+++ scripts/data_scraper.js
@@ -2,6 +2,8 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 const { exit } = require("process");
+const { rejects } = require("assert");
+const { url } = require("inspector");
 
 const home_page =
   "https://www.allrecipes.com/recipes/695/world-cuisine/asian/chinese/";
@@ -9,7 +11,7 @@ const chrome_path = "/usr/bin/google-chrome";
 
 const waitOptions = {
   waitUntil: "networkidle2",
-  timeout: 30000,
+  timeout: 60000,
 };
 
 // const scroll = async (page) => {
@@ -51,66 +53,117 @@ const waitOptions = {
 const scrape = async (page) => {
   const all_recipes = [];
   try {
-    // try {
-    //     await scroll(page);
-    // } catch (e) {
-    //     console.log(e)
-    // }
-
     const all_recipes_hrefs = await page.$$eval(
       '[id *= "mntl-taxonomysc-article-list-group"] a',
       (cards) => cards.map((card) => card.href)
     );
     for (let h of all_recipes_hrefs) {
       await page.goto(h, waitOptions);
-      console.log("URL:", h);
-      const article_heading = String(
-        await page.$eval('h1[class *= "article-heading"]', (h1) => h1.innerHTML)
-      ).trim();
-      const description = String(
-        await page.$eval('p[class *= "article-subheading"]', (p) => p.innerHTML)
-      ).trim();
-      const recipe_ingredients = await page.$$eval(
-        '[id *= "mntl-structured-ingredients"] ul li',
-        (uls) => uls.reduce((i, li) => i + "|" + li.innerHTML, "")
-      );
-      const instructions = await page.$$eval(
-        '[class *= "recipe__steps"] ol li',
-        (ols) => ols.reduce((i, li) => i + "|" + li.innerHTML, "")
-      );
 
-      const image_href = String(
-        await page.$eval(
-          '[class *= "primary-image__media"] img',
-          (img) => img.src
-        )
-      ).trim();
-      console.log("image href", image_href);
-      const image_buffer = await page.evaluate(async (image_href) => {
-        return fetch(image_href, {
-          method: "GET",
-          mode: "no-cors",
-        }).then((r) => {
-          console.log(r.body);
-          debugger;
-          return r.blob();
-        });
-      }, image_href);
+      console.log("page url:", h);
+      let article_heading = null;
+      let description = null;
+      let recipe_ingredients = null;
+      let instructions = null;
+      let image_file_path = null;
 
-      console.log("IMAGE BUFFER", image_buffer);
-      // fs.writeFileSync(`./recipe_images/${article_heading.replace(/\s+/gm,'_')}`, image_buffer)
-      const abs_path = path.resolve(
-        `./recipe_images/${article_heading.replace(/\s+/gm, "_")}`
+      const heading_exists = page.$('h1[class *= "article-heading"]');
+      if (heading_exists) {
+        article_heading = String(
+          await page.$eval(
+            'h1[class *= "article-heading"]',
+            (h1) => h1.innerHTML
+          )
+        ).trim();
+      }
+
+      const description_exists = await page.$(
+        'p[class *= "article-subheading"]'
       );
+      if (description_exists) {
+        description = String(
+          await page.$eval(
+            'p[class *= "article-subheading"]',
+            (p) => p.innerHTML
+          )
+        ).trim();
+      }
+
+      const recipe_ingredients_exists = await page.$(
+        '[id *= "mntl-structured-ingredients"] ul li'
+      );
+      if (recipe_ingredients_exists) {
+        recipe_ingredients = await page.$$eval(
+          '[id *= "mntl-structured-ingredients"] ul li',
+          (uls) =>
+            uls.reduce(
+              (i, li) =>
+                i +
+                "|" +
+                String(li.innerText).replace(
+                  /[\n|DOTDASH MEREDITH VIDEO STUDIOS]+/gm,
+                  " "
+                ),
+              ""
+            )
+        );
+      }
+
+      const instructions_exist = await page.$(
+        '[class *= "recipe__steps"] ol li'
+      );
+      if (instructions_exist) {
+        instructions = await page.$$eval(
+          '[class *= "recipe__steps"] ol li',
+          (ols) =>
+            ols.reduce(
+              (i, li) =>
+                i +
+                "|" +
+                String(li.innerText).replace(
+                  /[\n|DOTDASH MEREDITH VIDEO STUDIOS]+/gm,
+                  " "
+                ),
+              ""
+            )
+        );
+      }
+
+      const image_exists = await page.$(
+        '[class *= "primary-image__media"] img'
+      );
+      if (image_exists) {
+        const image_href = String(
+          await page.$eval(
+            '[class *= "primary-image__media"] img',
+            (img) => img.src
+          )
+        ).trim();
+        const image_res = await page.goto(image_href, waitOptions);
+        const imageBuffer = await image_res.buffer();
+        const image_name = image_href.split("/")[
+          image_href.split("/").length - 1
+        ];
+        image_file_path = path.resolve(
+          __dirname,
+          "recipe_images",
+          `${String(article_heading).replace(/\s+/gm, "_").toLowerCase()}.${
+            image_name.split(".")[image_name.split(".").length - 1]
+          }`
+        );
+        fs.writeFileSync(image_file_path, imageBuffer, "base64");
+      }
+
       const recipe = {
-        image: abs_path,
+        image: image_file_path,
         recipeName: article_heading,
         description: description,
         ingredients: recipe_ingredients,
         rating: Math.floor(Math.random() * 5) + 1,
         instructions: instructions,
-        notes: "",
+        notes: description,
       };
+      console.log("RECIPE", recipe);
       all_recipes.push(recipe);
     }
     console.log(all_recipes);
@@ -122,12 +175,11 @@ const scrape = async (page) => {
 };
 
 (async () => {
-  let browser,
-    allScrapedData = {};
+  let browser;
   try {
     browser = await puppeteer.launch({
       headless: false,
-      devtools: true,
+      devtools: false,
       userDataDir: "./user_data_scraper",
       ignoreDefaultArgs: ["--enable-automation"],
       ignoreHTTPSErrors: true,
@@ -146,7 +198,8 @@ const scrape = async (page) => {
       deviceScaleFactor: 1,
     });
     await page.goto(home_page, waitOptions);
-    await scrape(page);
+    const all_recipes = await scrape(page);
+    fs.writeFileSync("./scraped_recipes.json", JSON.stringify(all_recipes));
   } catch (e) {
     console.log(e);
     exit(1);
