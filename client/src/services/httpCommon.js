9b15df621306a60ac62647beec7353947ecca3af
leoli7405@gmail.com <leoli7405@gmail.com>
2024-03-25 00:14:27 -0700

    updated nginx server configs, don't forget to update iptables address space to disable port numbers for services

--- client/src/services/httpCommon.js
+++ client/src/services/httpCommon.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const server_ip = "208.68.39.73";
 // constant variables
 const base =
   process.env.NODE_ENV === "development"
     ? "http://localhost:50150/v1/"
-    : "http://20.102.36.29:5000/v1/";
+    : `${server_ip}/v1/`;
 
 export default axios.create({
   baseURL: base,
