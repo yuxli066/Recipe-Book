237ae90443ab16976459f5d02fa2cf046371e39c
leoli7405@gmail.com <leoli7405@gmail.com>
2024-04-09 00:53:28 -0700

    should've fixed axios redirection issue & node_env overwrite issue, also fixed cors issue on server

--- client/src/services/httpCommon.js
+++ client/src/services/httpCommon.js
@@ -1,15 +1,31 @@
 import axios from "axios";
 
 const server_ip = "208.68.39.73";
+
 // constant variables
 const base =
-  process.env.NODE_ENV === "development"
+  process.env.REACT_APP_ENVIRONMENT === "development"
     ? "http://localhost:50150/v1/"
-    : `${server_ip}/v1/`;
+    : `https://${server_ip}/v1/`;
 
-export default axios.create({
+const http_instance = axios.create({
   baseURL: base,
   headers: {
     "Content-type": "application/json",
   },
 });
+
+http_instance.defaults.maxRedirects = 0; // Set to 0 to prevent automatic redirects
+http_instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && [301, 302].includes(error.response.status)) {
+      const redirectUrl = error.response.headers.location;
+      console.log("redirect URL:", redirectUrl);
+      return http_instance.get(redirectUrl);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default http_instance;
--- client/src/services/recipeService.js
+++ client/src/services/recipeService.js
@@ -3,15 +3,15 @@ import httpCommon from "./httpCommon";
 export const getRecipes = httpCommon
   .get("recipes")
   .then((res) => {
-    if (res.status === 200)
-        return res.data;
+    console.log(res);
+    if (res.status === 200) return res.data;
   })
   .catch((err) => console.error(err));
 
-export const getRecipe = (recipeName) => httpCommon
-  .get(`/recipe/${recipeName}`)
-  .then((res) => {
-      if (res.status === 200)
-          return res.data;
-  })
-  .catch((err) => console.error(err));
+export const getRecipe = (recipeName) =>
+  httpCommon
+    .get(`/recipe/${recipeName}`)
+    .then((res) => {
+      if (res.status === 200) return res.data;
+    })
+    .catch((err) => console.error(err));
